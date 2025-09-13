39bb0ed563f946b2cec52f535c63671c733918af
Lilly-Fox <lfo61@uclive.ac.nz>
2025-09-13 15:01:54 +1200

    Update transient_class_properties.py

--- space_checker/transient_class_properties.py
+++ space_checker/transient_class_properties.py
@@ -1,19 +1,19 @@
 
 
 
-class flare_star():
-	def __init__(self,data,catalog):
-		self.stats = data 
+# class flare_star():
+# 	def __init__(self,data,catalog):
+# 		self.stats = data 
 
-	def _duration_prior(self):
+# 	def _duration_prior(self):
 
-		duration = self.stats.duration.values
-		self.duration_prob = duration < 2/24
+# 		duration = self.stats.duration.values
+# 		self.duration_prob = duration < 2/24
 
-	def _decline_prior(self):
-		return
+# 	def _decline_prior(self):
+# 		return
 
-	def _multi_event_prior(self):
-		if 
-		self.active = 
+# 	def _multi_event_prior(self):
+# 		if 
+# 		self.active = 
 
